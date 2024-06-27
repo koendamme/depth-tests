d81c38b5967da8f492b3ef248aa610524d3720a6
koendamme <koen.damme1999@gmail.com>
2024-06-27 15:36:58 +0200

    A lot of training and stuff

--- GAN/train_cProGAN.py
+++ GAN/train_cProGAN.py
@@ -1,6 +1,6 @@
 import torch
-from GAN.dataset import PreiswerkDataset, CustomDataset
-from GAN.utils import denormalize_tensor, scale_generator_output, create_video
+from dataset import CustomDataset
+from utils import denormalize_tensor, scale_generator_output, create_video
 from torch.utils.data import DataLoader, Subset
 import matplotlib.pyplot as plt
 from models.cProGAN import Generator, Discriminator, ConditionalProGAN
@@ -11,38 +11,55 @@ import math
 import wandb
 from torchvision.utils import save_image
 import os
-from GAN.dataset_splitter import DatasetSplitter
+from dataset_splitter import DatasetSplitter
 import numpy as np
+import gc
 
 
-def main():
+def eval(cProGAN, val_patterns, val_loaders):
+    vids = []
+    ssim_res, nmse_res = {}, {}
+    for pattern, val_loader in zip(val_patterns, val_loaders):
+        fake_imgs, real_imgs, nmse, ssim = cProGAN.evaluate(val_loader)
+        ssim_res[pattern] = ssim.mean()
+        nmse_res[pattern] = nmse.mean()
+        vid = np.concatenate([fake_imgs, real_imgs[:, None, :, :]], axis=3)
+        vid = np.uint8((vid + 1) / 2 * 255).repeat(3, axis=1)
+        vids.append(wandb.Video(vid, fps=5, caption=pattern))
+
+    return vids, ssim_res, nmse_res
+
+
+def train(subject):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     config = dict(
-        batch_size=24,
-        n_epochs=60,
+        batch_size=12,
+        n_epochs=120,
         desired_resolution=192,
-        noise_vector_length=128,
-        G_learning_rate=0.0001,
-        D_learning_rate=0.0001,
-        GP_lambda=10,
-        n_critic=1,
-        patient="A",
-        surrogates="MR Wave"
+        noise_vector_length=32,
+        G_learning_rate=0.001,
+        D_learning_rate=0.001,
+        GP_lambda=40,
+        n_critic=3,
+        patient=subject,
+        surrogates="All",
+        D_layers= [8, 32, 64, 128, 256, 512],
+        G_layers = [512, 256, 128, 64, 32, 8]
     )
 
-    wandb.init(project="CustomData-cProGAN-mr_surrogate", config=config)
+    run = wandb.init(project=f"CustomData-cProGAN-All_Surrogates", config=config, tags=[subject])
 
-    surrogate_freq, mri_freq = 50, 2.9
-    dataset = CustomDataset("C:\data", config["patient"], (500, 1000), int(surrogate_freq//mri_freq))
+    data_root = os.path.join("C:", os.sep, "data", "Formatted_datasets")
+    dataset = CustomDataset(data_root, config["patient"])
     splitter = DatasetSplitter(dataset, .8, .1, .1)
 
-    train_dataloader = DataLoader(splitter.get_train_dataset(), batch_size=config["batch_size"], shuffle=True)
+    train_dataloader = DataLoader(splitter.get_train_dataset(), batch_size=config["batch_size"], shuffle=True, pin_memory=True)
 
     val_patterns = ["Regular Breathing", "Shallow Breathing", "Deep Breathing"]
     val_loaders = []
     for pattern in val_patterns:
-        loader = DataLoader(splitter.val_subsets[pattern], batch_size=10, shuffle=False)
+        loader = DataLoader(splitter.val_subsets[pattern], batch_size=10, shuffle=False, pin_memory=True)
         val_loaders.append(loader)
 
     cProGAN = ConditionalProGAN(
@@ -52,26 +69,47 @@ def main():
         G_lr=config["G_learning_rate"],
         D_lr=config["D_learning_rate"],
         n_critic=config["n_critic"],
+        n_epochs=config["n_epochs"],
+        total_surrogate_length=dataset.signals_between_mrs*3,
+        D_layers=config["D_layers"],
+        G_layers=config["G_layers"]
     )
 
     for i in range(config["n_epochs"]):
         start_time = time.time()
-        D_loss, G_loss = cProGAN.train_single_epoch(train_dataloader, config["n_epochs"], i, gp_lambda=config["GP_lambda"])
+        D_loss, G_loss = cProGAN.train_single_epoch(train_dataloader, i, gp_lambda=config["GP_lambda"])
         end_time = time.time()
+        vids, ssim, nmse = eval(cProGAN, val_patterns, val_loaders)
 
-        vids = []
-        for pattern, val_loader in zip(val_patterns, val_loaders):
-            fake_imgs, real_imgs, _ = cProGAN.evaluate(val_loader)
-            video = create_video(fake_imgs, real_imgs)
-            vids.append(wandb.Video(video, fps=5, caption=pattern))
+        model_save_path = os.path.join("model_checkpoints", run.name)
+
+        if cProGAN.curr_step == cProGAN.total_steps - 1:
+            if not os.path.exists(model_save_path):
+                os.mkdir(model_save_path)
+            torch.save(cProGAN.G.state_dict(), os.path.join(model_save_path, f"Epoch{i}.pth"))
+            artifact = wandb.Artifact('model', type='model')
+            artifact.add_file(os.path.join(model_save_path, f"Epoch{i}.pth"))
+            run.log_artifact(artifact)
 
         wandb.log({
             "D_loss": D_loss,
             "G_loss": G_loss,
             "Epoch": i,
             "Training_epoch_time": end_time - start_time,
-            "Videos": vids
+            "Videos": vids,
+            "SSIM": ssim,
+            "NMSE": nmse
         })
+
+        del vids
+        gc.collect()
+    run.finish()
+
+
+def main():
+    subjects = ["B1", "B2", "B3", "C1", "C2", "C3"]
+    for s in subjects:
+        train(s)
 
 
 if __name__ == '__main__':
--- feature_extractors/mri/breathing_pattern_splitter.py
+++ feature_extractors/mri/breathing_pattern_splitter.py
@@ -7,23 +7,16 @@ from matplotlib.backend_bases import MouseButton
 
 
 def main():
-    # if not os.path.exists("temp_waveform.pickle"):
-    #     mri_images = extract_images(root_dir=r"C:\data\MRI-28-5\MRI")
-    #     mri_waveform = extract_waveform(mri_images)
-    #     with open('temp_waveform.pickle', 'wb') as f:
-    #         pickle.dump(mri_waveform, f)
-    # else:
-    #     with open('temp_waveform.pickle', 'rb') as f:
-    #         mri_waveform = pickle.load(f)
+    root = os.path.join("C:", os.sep, "data", "Formatted_datasets")
+    subjects = ["A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
 
-    with open(os.path.join("C:", os.sep, "data", "A", "mri_waveform.pickle"), 'rb') as f:
-        mri_waveform = pickle.load(f)
+    for s in subjects:
+        with open(os.path.join(root, s, "mr_wave.pickle"), 'rb') as f:
+            mri_waveform = pickle.load(f)["mri_waveform"]
 
-    d = split_dataset(mri_waveform)
-    with open(os.path.join("C:", os.sep, "data", "A", "splits.pickle"), 'wb') as file:
-        pickle.dump(d, file)
-
-    print(d)
+        d = split_dataset(mri_waveform)
+        with open(os.path.join(root, s, "splits.pickle"), 'wb') as file:
+            pickle.dump(d, file)
 
 
 def split_dataset(mri_waveform):
@@ -34,10 +27,10 @@ def split_dataset(mri_waveform):
         plt.plot(mri_waveform)
         plt.title(pattern)
         if i == 0:
-            points = plt.ginput(2, mouse_add=MouseButton.RIGHT, mouse_pop=None)
+            points = plt.ginput(2, mouse_add=MouseButton.RIGHT, mouse_pop=None, timeout=100)
             data[pattern] = {"start": int(points[0][0]), "end": int(points[1][0])}
         else:
-            points = plt.ginput(1, mouse_add=MouseButton.RIGHT, mouse_pop=None)
+            points = plt.ginput(1, mouse_add=MouseButton.RIGHT, mouse_pop=None, timeout=100)
             data[pattern] = {"start": data[patterns[i-1]]["end"] + 1, "end": int(points[0][0])}
 
         plt.close()
